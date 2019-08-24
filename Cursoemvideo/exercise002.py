26446c58a08e0c2d3cf62938067d16a684da8c38
Pedro Ottoni Carneiro <pedrottoni@outlook.com>
2019-08-24 00:26:35 -0300

    adicionando o exercício 002

---
+++ Cursoemvideo/exercise002.py
@@ -0,0 +1,10 @@
+'''Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.'''
+# Comente as linhas da forma que você não dezeja visualizar 
+
+# Forma 1
+userName = input('Digite seu nome:')
+print('Boas vindas,', x)
+
+# Forma 2
+userName = input('Digite seu nome:')
+print('Boas vindas, {}!'.format(userName))
