bff1d65b200fc749d36ef27f2f2f775369ca0aa9
Pedrottoni <pedrottoni@outlook.com>
2019-09-30 20:54:19 -0300

    Exercise 36 and Jogo da forca

---
+++ Cursoemvideo/exercise036.py
@@ -0,0 +1,18 @@
+'''
+Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+'''
+house_price = float(input('Qual o valor da casa? '))
+buyer_salary = float(input('Qual o salário do comprador? '))
+years_paying = int(input('Quantos anos de financiamento? '))
+
+monthly_installment = house_price / (years_paying * 12)
+thirty_percent = 30 / 100 * buyer_salary
+
+print(
+    f'Para pagar uma casa de R${house_price:.2f} em {int(years_paying)} anos, a prestação será de R${monthly_installment:.2f}'
+)
+
+if monthly_installment <= thirty_percent:
+    print('Emprestimno concedido')
+else:
+    print(f'Eprestimno negado')
