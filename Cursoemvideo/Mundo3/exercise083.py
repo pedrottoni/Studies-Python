32e88a2254d3914cdf708e773eacd9586e3f6842
Pedrottoni <pedrottoni@outlook.com>
2019-11-05 15:42:03 -0300

    ex 83

---
+++ Cursoemvideo/Mundo3/exercise083.py
@@ -0,0 +1,20 @@
+"""
+Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+"""
+bracket = []
+
+expression = str(input("Digite sua expressão: "))
+
+for element in expression:
+    if element == "(":
+        bracket.append(element)
+    if bracket.count("(") == 0:
+        bracket.append(")")
+        break
+    elif element == ")":
+        bracket.pop()
+
+if len(bracket) != 0:
+    print("Expressão inválida")
+else:
+    print("Expressão válida")
