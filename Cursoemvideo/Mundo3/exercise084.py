36728f79a36c82e5632a47da35c7ee4b45716b28
Pedrottoni <pedrottoni@outlook.com>
2019-11-06 21:16:52 -0300

    Arrumando o nome do 84 para 85

--- Cursoemvideo/Mundo3/exercise084.py
+++ Cursoemvideo/Mundo3/exercise084.py
@@ -1,17 +1,47 @@
 """
-Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:
+A) Quantas pessoas foram cadastradas.
+B) Uma listagem com as pessoas mais pesadas.
+C) Uma listagem com as pessoas mais leves.
 """
-from random import randint
+user = []
+users_list = []
 
-list = [[], []]
+higher_weight = True
+higher_weight_users = []
+lower_weight = True
+lower_weight_users = []
+choice = True
+print(lower_weight)
 
-for number in range(0, 7):
-    number = randint(0, 10)
-    if number % 2 == 0:
-        list[0].append(number)
-        list[0].sort()
-    else:
-        list[1].append(number)
-        list[1].sort()
+while choice != "0":
+    choice = input(
+        f"{'-'*100}\n{users_list}\nAo todo você cadastrou: {len(users_list)} usuários\nCadastrar nova pessoa:[1]  Mostrar maior peso:[2] Mostrar menor peso:[3] Sair [0]\n{'-'*100}\nEscolha: ")
 
-print(list)
+    if choice == "1":
+        name = str(input("Nome: "))
+        weight = float(input("Peso: "))
+        user = [name, weight]
+        users_list.append(user)
+
+        if weight > higher_weight:
+            higher_weight = weight
+            higher_weight_users.clear()
+            higher_weight_users.append(user[0])
+        elif weight == higher_weight:
+            higher_weight_users.append(user[0])
+
+        if weight < lower_weight or lower_weight == True:
+            lower_weight = weight
+            lower_weight_users.clear()
+            lower_weight_users.append(user[0])
+        elif weight == lower_weight:
+            lower_weight_users.append(user[0])
+
+    elif choice == "2":
+        print(
+            f"Maior peso: {higher_weight}\nUsuários com o maior peso: {higher_weight_users}")
+
+    elif choice == "3":
+        print(
+            f"Menor peso: {lower_weight}\nUsuários com o menor peso: {lower_weight_users}")
